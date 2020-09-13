9cae703d293bed985a554437a58a74c9c97ad5ca
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-13 20:00:54 +0600

    Add files via upload

---
+++ main8.js
@@ -0,0 +1,30 @@
+
+var letter=prompt("enter the letter: ")  ;
+letter=letter.toLowerCase();
+
+switch(letter){
+    case "a":
+        document.write("The letter is Vowel");
+        break;
+    case "e":
+        document.write("The letter is Vowel");
+        break;    
+    case "i":
+        document.write("The letter is Vowel");
+        break;   
+
+    case "o":
+        document.write("The letter is Vowel");
+        break;
+
+     case "u":
+        document.write("The letter is Vowel");
+        break;
+
+   default:
+       document.write("The letter is consonant");
+
+
+
+}
+
